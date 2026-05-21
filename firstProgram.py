cac6c8cad2eae858c5716ecc2ac008dafd2fae3f
Naveen <naveenkrishnawodeyar@gmail.com>
2026-05-22 01:59:28 +0530

    feat: print text using print()

---
+++ firstProgram.py
@@ -0,0 +1,19 @@
+# Python syntax, statements
+
+print("Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read:")
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+# SyntaxError: invalid syntax
+# print("Python is fun!") print("Really!")
+
+
+
+# Python Input
+# print() function to display text or output values
+print('Each call prints text on a new line by defaul')
+print("Text in Python must be inside quotes. You can use either double quotes or single quotes:")
+
+
+# Print Without a New Line
+print("Hello World!", end="  ")
+print("I will print on the same line.")
