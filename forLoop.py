973ebb9ffdc798a065249f83927d6cb216bf0b1e
Naveen <naveenkrishnawodeyar@gmail.com>
2026-05-30 00:22:43 +0530

    feat: for loop

---
+++ forLoop.py
@@ -0,0 +1,15 @@
+# -----------------------------------------
+# Python For Loop Demo Program
+# -----------------------------------------
+# A for loop is used to repeat a block
+# of code a fixed number of times.
+# -----------------------------------------
+
+# Loop from 1 to 5
+for number in range(1, 6):
+
+    # Print the current value
+    print("Number =", number)
+
+# This statement runs after the loop ends
+print("Loop finished!")
